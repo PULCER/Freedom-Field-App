0c317f35b02c438272db6a1719c9621d95395900
PULCER <anthony@pulcer.net>
2024-01-12 19:16:34 -0800

    newrefactorworking

--- Freedom Field App/FreedomViewModel.swift
+++ Freedom Field App/FreedomViewModel.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class FreedomViewModel: ObservableObject {
     @Published var isClockedIn = false
-    @Published var selectedTeam: String = "" 
+    @Published var selectedTeam: String = ""
 
     var events: [Event] = [
         Event(team: "Team A", startTime: "8 AM", endTime: "12 PM"),
@@ -13,5 +13,42 @@ class FreedomViewModel: ObservableObject {
     func eventForCurrentTeam() -> Event? {
         events.first { $0.team == selectedTeam }
     }
-}
 
+    func currentTimeFormatted() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h a"
+        return dateFormatter.string(from: Date())
+    }
+
+    func minutesSince5AM(from time: String) -> Int {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h a"
+
+        guard let date = formatter.date(from: time) else { return 0 }
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute], from: date)
+        let hours = components.hour ?? 0
+        let minutes = components.minute ?? 0
+
+        return (hours - 5) * 60 + minutes
+    }
+
+    func generateTimeSlots() -> [(String, Bool)] {
+        var slots = [(String, Bool)]()
+        for hour in 5..<22 {
+            let hourString = "\(hour <= 12 ? hour : hour - 12) \(hour < 12 ? "AM" : "PM")"
+            slots.append((hourString, true))  // True for full hour
+            slots.append(("", false))  // False for half hour
+        }
+        return slots
+    }
+
+    func isEventTimeSlot(event: Event, time: String, isHour: Bool) -> Bool {
+        let eventStart = minutesSince5AM(from: event.startTime)
+        let eventEnd = minutesSince5AM(from: event.endTime)
+        let slotStart = minutesSince5AM(from: time)
+        let slotEnd = slotStart + (isHour ? 60 : 30)  // 60 minutes for full hour, 30 for half
+
+        return slotStart < eventEnd && slotEnd > eventStart
+    }
+}
--- Freedom Field App/Views/CalanderView.swift
+++ Freedom Field App/Views/CalanderView.swift
@@ -4,67 +4,25 @@ struct CalendarView: View {
     @ObservedObject var viewModel: FreedomViewModel
 
     var body: some View {
-           ScrollView {
-               VStack(spacing: 0) {
-                   ForEach(generateTimeSlots(), id: \.self.0) { (time, isHour) in
-                       ZStack(alignment: .top) {
-                           TimeSlotView(time: time, isHour: isHour, isCurrentTime: time == currentTimeFormatted())
+        ScrollView {
+            VStack(spacing: 0) {
+                ForEach(viewModel.generateTimeSlots(), id: \.self.0) { (time, isHour) in
+                    ZStack(alignment: .top) {
+                        TimeSlotView(time: time, isHour: isHour, isCurrentTime: time == viewModel.currentTimeFormatted())
 
-                           if let event = viewModel.eventForCurrentTeam(), isEventTimeSlot(event: event, time: time, isHour: isHour) {
-                               Rectangle()
-                                   .fill(Color.blue.opacity(0.3))
-                                   .frame(height: isHour ? 30 : 15) // Adjusted slot height
-                           }
-                       }
-                       .frame(height: isHour ? 30 : 15) // Adjusted slot height
-                   }
-               }
-           }
+                        if let event = viewModel.eventForCurrentTeam(), viewModel.isEventTimeSlot(event: event, time: time, isHour: isHour) {
+                            Rectangle()
+                                .fill(Color.blue.opacity(0.3))
+                                .frame(height: isHour ? 30 : 15)
+                        }
+                    }
+                    .frame(height: isHour ? 30 : 15)
+                }
+            }
+        }
            .padding()
            .frame(maxWidth: .infinity, maxHeight: .infinity)
            .border(Color.gray, width: 1)
            .padding()
        }
 }
-
-extension CalendarView {
-    func currentTimeFormatted() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "h a"
-        return dateFormatter.string(from: Date())
-    }
-
-    func minutesSince5AM(from time: String) -> Int {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "h a"
-
-        guard let date = formatter.date(from: time) else { return 0 }
-        let calendar = Calendar.current
-        let components = calendar.dateComponents([.hour, .minute], from: date)
-        let hours = components.hour ?? 0
-        let minutes = components.minute ?? 0
-
-        return (hours - 5) * 60 + minutes
-    }
-
-    func generateTimeSlots() -> [(String, Bool)] {
-        var slots = [(String, Bool)]()
-        for hour in 5..<22 {
-            let hourString = "\(hour <= 12 ? hour : hour - 12) \(hour < 12 ? "AM" : "PM")"
-            slots.append((hourString, true))  // True for full hour
-            slots.append(("", false))  // False for half hour
-        }
-        return slots
-    }
-
-    func isEventTimeSlot(event: Event, time: String, isHour: Bool) -> Bool {
-        let eventStart = minutesSince5AM(from: event.startTime)
-        let eventEnd = minutesSince5AM(from: event.endTime)
-        let slotStart = minutesSince5AM(from: time)
-        let slotEnd = slotStart + (isHour ? 60 : 30)  // 60 minutes for full hour, 30 for half
-
-        return slotStart < eventEnd && slotEnd > eventStart
-    }
-}
-
-
